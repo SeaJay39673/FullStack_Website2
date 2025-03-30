64781dc010d0df2f8edc25407e7b0712613d46ef
seajay39673 <chris.easy15@gmail.com>
2025-03-30 11:23:13 -0600

    update: connect to db

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
 const express = require("express")
 const app = express()
 const PORT = 7000
@@ -6,8 +8,17 @@ app.get("/", (req, res) => {
     res.send("Hello, World")
 })
 
-app.listen(PORT, (err) => {
-    if (err)
+async function start() {
+    try {
+        await mongoose.connect(process.env.ATLAS_URI)
+        app.listen(PORT, (err) => {
+            if (err)
+                console.log(err)
+            console.log(`App listening on port ${PORT}`)
+        })
+    } catch (err) {
         console.log(err)
-    console.log(`App listening on port ${PORT}`)
-})
+    }
+}
+
+start()
